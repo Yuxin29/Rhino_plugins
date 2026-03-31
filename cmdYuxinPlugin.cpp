e5294f37a4d09a273d79e4e798b156e0b8c71272
Yuxin29 <yuxin.29.wu@gmail.com>
2026-03-31 13:35:04 +0300

    initialized

---
+++ cmdYuxinPlugin.cpp
@@ -0,0 +1,98 @@
+// cmdYuxinPlugin.cpp : command file
+//
+
+#include "stdafx.h"
+#include "YuxinFirstPluginPlugIn.h"
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
+// BEGIN YuxinPlugin command
+//
+
+#pragma region YuxinPlugin command
+
+// Do NOT put the definition of class CCommandYuxinPlugin in a header
+// file. There is only ONE instance of a CCommandYuxinPlugin class
+// and that instance is the static theYuxinPluginCommand that appears
+// immediately below the class definition.
+
+class CCommandYuxinPlugin : public CRhinoCommand
+{
+public:
+  // The one and only instance of CCommandYuxinPlugin is created below.
+  // No copy constructor or operator= is required.
+  // Values of member variables persist for the duration of the application.
+
+  // CCommandYuxinPlugin::CCommandYuxinPlugin()
+  // is called exactly once when static theYuxinPluginCommand is created.
+  CCommandYuxinPlugin() = default;
+
+  // CCommandYuxinPlugin::~CCommandYuxinPlugin()
+  // is called exactly once when static theYuxinPluginCommand is destroyed.
+  // The destructor should not make any calls to the Rhino SDK. 
+  // If your command has persistent settings, then override 
+  // CRhinoCommand::SaveProfile and CRhinoCommand::LoadProfile.
+  ~CCommandYuxinPlugin() = default;
+
+  // Returns a unique UUID for this command.
+  // If you try to use an id that is already being used, then
+  // your command will not work. Use GUIDGEN.EXE to make unique UUID.
+  UUID CommandUUID() override
+  {
+    // {D6304EA5-26E5-4769-B21C-4D1C9608B116}
+    static const GUID YuxinPluginCommand_UUID = 
+    {0xd6304ea5,0x26e5,0x4769,{0xb2,0x1c,0x4d,0x1c,0x96,0x08,0xb1,0x16}};
+    return YuxinPluginCommand_UUID;
+  }
+
+  // Returns the English command name.
+  // If you want to provide a localized command name, then override 
+  // CRhinoCommand::LocalCommandName.
+  const wchar_t* EnglishCommandName() override { return L"YuxinPlugin"; }
+
+  // Rhino calls RunCommand to run the command.
+  CRhinoCommand::result RunCommand(const CRhinoCommandContext& context) override;
+};
+
+// The one and only CCommandYuxinPlugin object
+// Do NOT create any other instance of a CCommandYuxinPlugin class.
+static class CCommandYuxinPlugin theYuxinPluginCommand;
+
+CRhinoCommand::result CCommandYuxinPlugin::RunCommand(const CRhinoCommandContext& context)
+{
+  // CCommandYuxinPlugin::RunCommand() is called when the user
+  // runs the "YuxinPlugin".
+
+  // TODO: Add command code here.
+
+  // Rhino command that display a dialog box interface should also support
+  // a command-line, or script-able interface.
+
+  ON_wString str;
+  str.Format(L"The \"%s\" command is under construction.\n", EnglishCommandName());
+  const wchar_t* pszStr = static_cast<const wchar_t*>(str);
+  if (context.IsInteractive())
+    RhinoMessageBox(pszStr, YuxinFirstPluginPlugIn().PlugInName(), MB_OK);
+  else
+    RhinoApp().Print(pszStr);
+
+  // TODO: Return one of the following values:
+  //   CRhinoCommand::success:  The command worked.
+  //   CRhinoCommand::failure:  The command failed because of invalid input, inability
+  //                            to compute the desired result, or some other reason
+  //                            computation reason.
+  //   CRhinoCommand::cancel:   The user interactively canceled the command 
+  //                            (by pressing ESCAPE, clicking a CANCEL button, etc.)
+  //                            in a Get operation, dialog, time consuming computation, etc.
+
+  return CRhinoCommand::success;
+}
+
+#pragma endregion
+
+//
+// END YuxinPlugin command
+//
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
